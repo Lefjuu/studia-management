e18f050e13f651e2c52b3318c3005fe78c5d1d91
Lefjuu <karol.legut121@gmail.com>
2024-05-01 18:59:20 +0200

    feat: LoginResponse DTO with FullName property

--- dtos/LoginResponse.cs
+++ dtos/LoginResponse.cs
@@ -4,9 +4,10 @@ namespace MongoAuthenticatorAPI.Dtos
     public class LoginResponse
     {
         public bool Success { get; set; }
+        public string FullName { get; set; } = string.Empty;
         public string AccessToken { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string UserId { get; set; } = string.Empty;
+        public string Id { get; set; } = string.Empty;
         public string Message { get; set; } = string.Empty;
     }
 }
