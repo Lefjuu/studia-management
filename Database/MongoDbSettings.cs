07c119a9329b76838a42543fa39a26672761f4a6
Lefjuu <karol.legut121@gmail.com>
2024-05-01 18:58:55 +0200

    refactor: delete crap code

--- Database/MongoDbSettings.cs
+++
@@ -1,6 +0,0 @@
-public class MongoDbSettings
-{
-    public string ConnectionString { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-    public string UsersCollectionName { get; set; } = null!;
-}
--- Settings/MongoDbConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IdentityMongo.Settings
-{
-    public class MongoDbConfig
-    {
-        public string DatabaseName { get; init; }
-        public string ConnectionString { get; init; }
-    }
-}
