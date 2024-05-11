cacc038ddeb452e2ebedfe1a39b006dca7db884f
Lefjuu <karol.legut121@gmail.com>
2024-05-11 15:55:46 +0200

    feat: ApplicationUser model with properties and RoleEnum

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+
+public class RoleEnum
+{
+    public const string User = "user";
+    public const string Admin = "admin";
+}
+
+[CollectionName("Users")]
+public class ApplicationUser : MongoIdentityUser<Guid>
+{
+    public string FullName { get; set; }
+    public bool IsConnected { get; set; } = false;
+    public DateTime LastModified { get; set; } = DateTime.Now;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public string Role { get; set; } = "user";
+}
