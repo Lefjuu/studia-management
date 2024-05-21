f023e414e5d9b0c540f7235eacbefc436f0215a6
Lefjuu <karol.legut121@gmail.com>
2024-05-21 21:53:56 +0200

    refactor: program

--- Program.cs
+++ Program.cs
@@ -5,21 +5,20 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using MongoAuthenticatorAPI.Models;
+using MongoAuthenticatorAPI.Services;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String)); // General setting
-
-// BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeSerializer(MongoDB.Bson.BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
 
-
-//add mongoIdentityConfiguration...
+// Add MongoDB identity configuration
 var mongoDbIdentityConfig = new MongoDbIdentityConfiguration
 {
     MongoDbSettings = new MongoDbSettings
@@ -30,40 +29,38 @@ var mongoDbIdentityConfig = new MongoDbIdentityConfiguration
     IdentityOptionsAction = options =>
     {
         options.Password.RequireDigit = false;
-        // options.Password.RequiredLength = 8;
         options.Password.RequireNonAlphanumeric = true;
         options.Password.RequireLowercase = false;
 
-        //lockout
+        // Lockout
         options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
         options.Lockout.MaxFailedAccessAttempts = 5;
 
         options.User.RequireUniqueEmail = true;
-
     }
-
 };
 
 builder.Services.ConfigureMongoDbIdentity<ApplicationUser, ApplicationRole, Guid>(mongoDbIdentityConfig)
-    .AddUserManager<UserManager<ApplicationUser>>();
-// .AddSignInManager<SignInManager<ApplicationUser>>()
-// .AddRoleManager<RoleManager<ApplicationRole>>()
-// .AddDefaultTokenProviders();
+    .AddUserManager<UserManager<ApplicationUser>>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
     .AddDefaultTokenProviders();
+
+// var jwtSecretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY");
+var jwtSecretKey = "486897e25269fb7777a0376bf91ffc4e5ec9e199f52daff5053659182e0c96342a204da9643914dc78d2e4020b0dca42286a0d3b9dd29a34bb897dc8c588a032";
+
+if (string.IsNullOrEmpty(jwtSecretKey))
+{
+    throw new InvalidOperationException("JWT_SECRET_KEY environment variable is not set.");
+}
 
 builder.Services.AddAuthentication(x =>
 {
     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-
-
-
 }).AddJwtBearer(x =>
 {
-    var keyString = Environment.GetEnvironmentVariable("JWT_SECRET_KEY");
     x.RequireHttpsMetadata = true;
     x.SaveToken = true;
     x.TokenValidationParameters = new TokenValidationParameters
@@ -74,9 +71,8 @@ builder.Services.AddAuthentication(x =>
         ValidateLifetime = true,
         ValidIssuer = "https://localhost:5000",
         ValidAudience = "https://localhost:5000",
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("486897e25269fb7777a0376bf91ffc4e5ec9e199f52daff5053659182e0c96342a204da9643914dc78d2e4020b0dca42286a0d3b9dd29a34bb897dc8c588a032")),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecretKey)),
         ClockSkew = TimeSpan.Zero
-
     };
 });
 
@@ -92,27 +88,22 @@ builder.Services.AddCors(options =>
         });
 });
 
-
-
-
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>(); // Register the service
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     // app.UseSwagger();
     // app.UseSwaggerUI();
 }
+
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
 app.UseAuthentication();
-
 app.UseAuthorization();
 
 app.MapControllers();
