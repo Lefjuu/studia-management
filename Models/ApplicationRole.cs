dea0f0e54216a06550a8eaebf30a12a7077f6244
Lefjuu <karol.legut121@gmail.com>
2024-06-16 13:34:09 +0200

    refactor: delete unsued file

--- Models/ApplicationRole.cs
+++ Models/ApplicationRole.cs
@@ -3,6 +3,7 @@ using MongoDbGenericRepository.Attributes;
 
 namespace MongoAuthenticatorAPI.Models
 {
+    // default init on db
     [CollectionName("roles")]
     public class ApplicationRole : MongoIdentityRole<Guid>
     {
