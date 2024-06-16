4b0beb3b0b7e761239d7dbfb0480eb141c7683e2
sknytzs <pactwaf@gmail.com>
2024-06-16 11:49:26 +0200

    Add Role property to LoginResponse DTO

--- dtos/LoginResponse.cs
+++ dtos/LoginResponse.cs
@@ -9,6 +9,7 @@ namespace MongoAuthenticatorAPI.Dtos
         public string Email { get; set; } = string.Empty;
         public string Id { get; set; } = string.Empty;
         public string Message { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
     }
 }
 
