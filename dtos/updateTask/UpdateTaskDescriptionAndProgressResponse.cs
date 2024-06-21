1ca64ea22cc7a2d0c5aab64abd7e55417199d1cd
Lefjuu <karol.legut121@gmail.com>
2024-06-21 22:16:22 +0200

    refactor: TasksController routes and add UpdateTaskDescriptionAndProgressResponse class

---
+++ dtos/updateTask/UpdateTaskDescriptionAndProgressResponse.cs
@@ -0,0 +1,9 @@
+namespace MongoAuthenticatorAPI.Dtos
+{
+    public class UpdateTaskDescriptionAndProgressResponse
+    {
+        public string Description { get; set; }
+        public int Progress { get; set; }
+        public string Message { get; set; }
+    }
+}
